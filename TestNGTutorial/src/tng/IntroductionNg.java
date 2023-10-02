637fd9622e6ebbbddad463ab68a3a30dda930518
Vitor Sartorelli <vitor_goo@hotmail.com>
2023-10-01 21:55:58 -0300

    criacao do projeto tstng

---
+++ TestNGTutorial/src/tng/IntroductionNg.java
@@ -0,0 +1,18 @@
+package tng;
+
+import org.testng.annotations.Test;
+
+public class IntroductionNg {
+
+	@Test
+	public void Primeiro() {
+		System.out.println("Hello World");
+	}
+	/// ---- ///
+	@Test
+	public void Segundo() {
+		System.out.println("Bye");
+	}
+	
+	
+}
---
+++ TestNGTutorial/src/tng/IntroductionNg2.java
@@ -0,0 +1,11 @@
+package tng;
+
+import org.testng.annotations.Test;
+
+public class IntroductionNg2 {
+
+	@Test
+	public void Terceiro() {
+		System.out.println("Cowboy");
+	}
+}
